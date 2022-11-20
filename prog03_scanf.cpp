0acc8be65e33ab8093d11ef7d5b24b70b2465aed
Gael Garcia <31973040+Poppygael0@users.noreply.github.com>
2022-11-20 13:09:56 -0600

    Add files via upload

---
+++ prog03_scanf.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream> 
+
+using namespace std;
+
+int x,y,r;
+
+main(){
+    printf("Indica el valor de X :" ); scanf("%d,x");
+    printf("Indica el valor de y : "); scanf("%d",&y)
+    r=x+y;
+    printf("La suma de %d m%cs %d es : %d",x,160,y,r);
+    getche();
+
+
+}
